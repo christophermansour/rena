44061565f2192778294bbe169ee9c5fc08c997bb
ejelome <ejelome@gmail.com>
2017-01-07 01:49:36 +0100

    Add Tutorial

---
+++ 2-tutorial/tutorial/src/tutorial/db.cljs
@@ -0,0 +1,10 @@
+(ns tutorial.db
+  (:require [clojure.spec :as s]))
+
+;; spec of app-db
+(s/def ::greeting string?)
+(s/def ::app-db
+  (s/keys :req-un [::greeting]))
+
+;; initial state of app-db
+(def app-db {:greeting "Hello World!"})
---
+++ 2-tutorial/tutorial/src/tutorial/subs.cljs
@@ -0,0 +1,7 @@
+(ns tutorial.subs
+  (:require [re-frame.core :refer [reg-sub]]))
+
+(reg-sub
+  :get-greeting
+  (fn [db _]
+    (:greeting db)))
