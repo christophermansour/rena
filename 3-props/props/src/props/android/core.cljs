04a1a715d4c9bbd84197ed6e86fff24783319b6c
ejelome <ejelome@gmail.com>
2017-01-08 14:29:34 +0100

    Update core.cljs
    * Comment image example
    * Add text and reusable text xample

--- 3-props/props/src/props/android/core.cljs
+++ 3-props/props/src/props/android/core.cljs
@@ -2,13 +2,26 @@
   (:require [reagent.core :as r]))
 
 (def ReactNative (js/require "react-native"))
+
 (def app-registry (.-AppRegistry ReactNative))
-(def image (r/adapt-react-class (.-Image ReactNative)))
+;; (def image (r/adapt-react-class (.-Image ReactNative)))
+(def text (r/adapt-react-class (.-Text ReactNative)))
+(def view (r/adapt-react-class (.-View ReactNative)))
+
+(defn greeting [name]
+  [text "Hello " name "!"])
+
+(defn lots-of-greetings []
+  [view {:style {:align-items "center"}}
+   [greeting "Rexxar"]
+   [greeting "Jaina"]
+   [greeting "Valeera"]])
 
 (defn app-root []
-  (let [pic {:uri "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"}]
-    [image {:source pic
-            :style {:width 193 :height 110}}]))
+  ;; (let [pic {:uri "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"}]
+  ;;   [image {:source pic
+  ;;           :style {:width 193 :height 110}}])
+  [lots-of-greetings])
 
 (defn init []
   (.registerComponent app-registry "Props" #(r/reactify-component app-root)))
