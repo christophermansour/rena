34b785786a78508afa03339cd2fdd986bb829474
ejelome <ejelome@gmail.com>
2017-01-08 01:00:16 +0100

    Remove re-frame source

--- 2-tutorial/tutorial/src/tutorial/android/core.cljs
+++ 2-tutorial/tutorial/src/tutorial/android/core.cljs
@@ -1,18 +1,12 @@
 (ns tutorial.android.core
-  (:require [reagent.core :as r :refer [atom]]
-            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-            [tutorial.events]
-            [tutorial.subs]))
+  (:require [reagent.core :as r]))
 
 (def ReactNative (js/require "react-native"))
-
 (def app-registry (.-AppRegistry ReactNative))
 (def text (r/adapt-react-class (.-Text ReactNative)))
 
 (defn app-root []
-  (let [greeting (subscribe [:get-greeting])]
-    [text @greeting]))
+  [text "Hello world!"])
 
 (defn init []
-  (dispatch-sync [:initialize-db])
   (.registerComponent app-registry "Tutorial" #(r/reactify-component app-root)))
