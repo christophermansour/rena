182fd6035eddad193fdd7b46a20bc880efa011fb
ejelome <ejelome@gmail.com>
2017-01-15 04:09:41 +0100

    Remove tutorial

--- tutorial/env/prod/env/ios/main.cljs
+++
@@ -1,6 +0,0 @@
- (ns env.ios.main
-  (:require [tutorial.ios.core :as core]))
-
- (core/init)
-
-
--- tutorial/src/tutorial/subs.cljs
+++
@@ -1,7 +0,0 @@
-(ns tutorial.subs
-  (:require [re-frame.core :refer [reg-sub]]))
-
-(reg-sub
-  :get-greeting
-  (fn [db _]
-    (:greeting db)))
